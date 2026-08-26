1cf48769ce4136c2ba6740313346c743ee01f36e
Walther-P <ht500119es@gmail.com>
2026-08-26 10:11:23 +0800

    Add files via upload
    更新到1.2版

--- sw.js
+++ sw.js
@@ -1,5 +1,10 @@
-const CACHE='ktak-v11-shell';
-const SHELL=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request).then(resp=>{const copy=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return resp}).catch(()=>cached)))});
+const CACHE="ktak-v1-2-shell";
+const SHELL=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener("fetch",e=>{
+  if(e.request.method!=="GET") return;
+  e.respondWith(fetch(e.request).then(resp=>{
+    const copy=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return resp;
+  }).catch(()=>caches.match(e.request)));
+});
