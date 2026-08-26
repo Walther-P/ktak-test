f93fbbc181b4d743191587ea0f94acbf9260ab86
Walther-P <ht500119es@gmail.com>
2026-08-26 17:03:06 +0800

    Add files via upload
    V1.9.2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION="ktak-v191-retirement";
+const VERSION="ktak-v192-iosfix-retirement";
 self.addEventListener("install",e=>{self.skipWaiting()});
 self.addEventListener("activate",e=>{
   e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith("ktak-")).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))
